dddd4a76014845b36ec170766c9d2ba4b07335e7
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2023-09-13 17:06:17 -0700

    Fixed alot to get that working...

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -4,11 +4,19 @@
 if (!Spawn.prototype._spawnCreep) {
     Spawn.prototype._spawnCreep = Spawn.prototype.spawnCreep;
 
-    Spawn.prototype.spawnCreep = function() {
-        if (arguments[3] == undefined || arguments[3].memory == undefined || arguments[3].memory.home == undefined) {
-            arguments[3].memory.home = this.room.name;
+    Spawn.prototype.spawnCreep = function(...theArgs) {
+        if(theArgs.length < 3) {
+           theArgs.push({memory: {home: this.room.name}});
+        } else {
+            if (theArgs[3].memory == undefined) {
+                theArgs[3].memory = {home: this.room.name};
+            } else {
+                if (theArgs[3].memory.home == undefined) {
+                    theArgs[3].memory.home = this.room.name;
+                }
+            }
         }
 
-        return this._spawnCreep.apply(this, arguments);
+        return this._spawnCreep.apply(this, theArgs);
     }
 }
